dfcd6a15882ac26a6a6cfa48bbf69dfd35e10fba
Rio Zhu <zzhu@futurewei.com>
2021-12-08 16:57:08 -0800

    Comment out the panicing line, just to see what happens

--- cmd/main.go
+++ cmd/main.go
@@ -173,7 +173,7 @@ func runClient(){
 			if through_put_test_end_time.Sub(time.Now()).Milliseconds() <= 0 {
 				//test_stop_channel <- struct{}{}
 				ctx.Done()
-				log.Printf("Time to stop sending requests, requests sent: %d, request finished: %d, received GoalStateV2 amount: %d\n", request_id +1, count, &global_server_api_instance.Received_goalstatev2_count)
+				//log.Printf("Time to stop sending requests, requests sent: %d, request finished: %d, received GoalStateV2 amount: %d\n", request_id +1, count, &global_server_api_instance.Received_goalstatev2_count)
 				global_server.Stop()
 				global_client_connection.Close()
 				log.Println("Time's up, stop the server")
